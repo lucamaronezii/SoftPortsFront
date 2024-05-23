2334db9bdfd6580193de76f208e3d1c6591da65e
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-05-23 09:46:04 -0300

    antd

--- softportsfront/src/layouts/MainLayout.tsx
+++ softportsfront/src/layouts/MainLayout.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import LeftSidebar from './LeftSidebar'
 import { Outlet } from 'react-router-dom'
+import { Button, Flex } from 'antd'
 
 const MainLayout = () => {
     return (
-        <div>
+        <Flex>
             <LeftSidebar />
             <div style={{ flexGrow: 1 }}>
                 <Outlet />
             </div>
-        </div>
+        </Flex>
     )
 }
 
