858ec1a12edee9fc68ec29e7ee75c1591e80fff6
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-07-19 10:26:12 -0300

    closed issues initial development

--- softportsfront/src/pages/Projects/ClosedIssues/ClosedIssues.tsx
+++ softportsfront/src/pages/Projects/ClosedIssues/ClosedIssues.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import { Cascader, Flex, Input, Spin, Typography } from 'antd'
+import { issueFilterItems } from '../../../utils/issueFilterItems'
+import { useState } from 'react'
+import { CustomRow } from '../../../components/CustomRow/styles'
+import { CustomBox } from '../styles'
+import { NoIssuesBox } from '../OpenIssues/styles'
+import { BugFilled } from '@ant-design/icons'
+import { IIssue } from '../interfaces'
+import { issuesList } from '../../../mocks/Issues'
+import ClosedIssue from '../components/ClosedIssue/ClosedIssue'
 
-const FixedIssues = () => {
+const OpenIssues = () => {
+  const [input, setInput] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+  const [issues, setIssues] = useState<IIssue[]>(issuesList)
+
   return (
-    <div>
-      fixed
-    </div>
+    <CustomBox style={{ height: '85vh' }}>
+      <CustomRow>
+        <Flex gap={15}>
+          <div style={{ maxWidth: '300px' }}>
+            <Input.Search
+              value={input}
+              placeholder='Pesquisar registro'
+              allowClear
+              enterButton
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+          <Cascader
+            removeIcon
+            placeholder='Filtrar registros'
+            multiple
+            options={issueFilterItems}
+            maxTagCount={'responsive'}
+          />
+        </Flex>
+      </CustomRow>
+
+      {loading ? (
+        <Spin size='large' style={{ marginTop: 35 }} />
+      ) : (
+        issues && issues.length > 0 ? (
+          issues.map((issue, index) => (
+            <ClosedIssue 
+              key={index}
+              id={issue.id}
+              classificacoes={issue.classificacoes}
+              dataCorrecao={issue.dataCorrecao}
+              descricao={issue.descricao}
+              prioridade={issue.prioridade}
+              responsaveis={issue.responsaveis}
+              status={issue.status}
+              titulo={issue.titulo}
+              caminho={issue.caminho}
+              casosDeTestes={issue.casosDeTestes}
+            />
+          ))
+        ) : (
+          <NoIssuesBox>
+            <BugFilled style={{ fontSize: 40 }} />
+            <Typography.Title level={4}>
+              Nenhum issue fechado encontrado. Feche issues para visualizá-los
+            </Typography.Title>
+          </NoIssuesBox>
+        )
+      )}
+    </CustomBox>
   )
 }
 
-export default FixedIssues
+export default OpenIssues
