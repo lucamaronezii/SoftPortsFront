6054602dc416068a1997cbbb3fe1644bb0a9d2f8
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-07-19 11:08:43 -0300

    .

--- softportsfront/src/styles/theme.ts
+++ softportsfront/src/styles/theme.ts
@@ -1,7 +1,7 @@
 import { MapToken } from "antd/es/theme/interface";
 import { ComponentsToken } from "antd/es/theme/context";
 
-export const prColor = "#1CAE9D"
+export const prColor = "#00A19F" // "#1CAE9D" (older)
 export const secBgColor = "#252426"
 export const plcColor = "#858585"
 export const errColor = "#FB223B"
