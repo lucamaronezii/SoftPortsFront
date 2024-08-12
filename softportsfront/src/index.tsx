15c2d60535013f0f336e4332a31cfbb22def49b7
Luca Maronezi Steyer <steyerluca@hotmail.com>
2024-08-11 23:10:06 -0300

    keycloak

--- softportsfront/src/index.tsx
+++ softportsfront/src/index.tsx
@@ -9,6 +9,7 @@ import GlobalContext from './context/GlobalContext';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ConfigProvider>
--- softportsfront/src/routes/Router.tsx
+++ softportsfront/src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from '../pages/Dashboard/Dashboard'
 import MainLayout from '../layouts/MainLayout'
 import NotFound from '../pages/NotFound/NotFound'
@@ -7,13 +7,15 @@ import Projects from '../pages/Projects/Projects'
 import Users from '../pages/Users/Users'
 import Configurations from '../pages/Configurations/Configurations'
 import Login from '../pages/Login/Login'
+import useAuth from '../hooks/useAuth'
 
 const Router = () => {
+    const isLogged = useAuth()
 
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <MainLayout />,
+            element: isLogged ? <MainLayout /> : <Navigate to={'/login'} />,
             errorElement: <NotFound />,
             children: [
                 { path: '/', element: <Dashboard /> },
